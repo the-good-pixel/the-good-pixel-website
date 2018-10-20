26e111bd3fd375e8d91589f0c398876abbbea202
Anthony Chiu <kingychiu@gmail.com>
2018-10-20 22:58:35 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const dom = require('gulp-dom');
+const concat = require("gulp-concat");
 
 gulp.task('unify-core-sass', function () {
-    return gulp.src('./public/unify-template-core-assets/include/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/unify-template-core-assets/css'));
+  return gulp.src('./public/unify-template-core-assets/include/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/unify-template-core-assets/css'));
 });
 
 gulp.task('page-sass', function () {
-    return gulp.src('./public/assets/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/assets/css'));
+  return gulp.src('./public/assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/assets/css'));
 });
 
-gulp.task('default', ['unify-core-sass', 'page-sass']);
+
+gulp.task('default', gulp.parallel('unify-core-sass', 'page-sass'), function () {
+  return;
+});
